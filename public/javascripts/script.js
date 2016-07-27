d1ca55038a3467a66a481dcb43601790bff83426
Ivan Chau <ivanmchau@gmail.com>
2016-07-27 17:53:31 -0400

    map respon

---
+++ public/javascripts/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+        // you want to enable the pointer events only on click;
+
+        $('#map').addClass('scrolloff'); // set the pointer events to none on doc ready
+        $('#over').on('click', function () {
+            $('#map').removeClass('scrolloff'); // set the pointer events true on click
+        });
+
+        // you want to disable pointer events when the mouse leave the canvas area;
+
+        $("#map").mouseleave(function () {
+            $('#map').addClass('scrolloff'); // set the pointer events to none when mouse leaves the map area
+        });
+});
